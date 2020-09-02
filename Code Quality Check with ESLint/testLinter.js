167a24ccc54f781d83c61d1e166442b5e6ce4e90
Yash Maheshwari <yash@gmail.com>
2020-09-02 10:57:18 +0530

    eslint

---
+++ Code Quality Check with ESLint/testLinter.js
@@ -0,0 +1,10 @@
+const calculator = require('./calculator');
+
+let a = 12;
+let b = 9
+
+console.log(` a + b = ${calculator.add(a,b)}`);
+
+console.log(`a - b = ${calculator.subtract(a,b)}`);
+
+console.log(`a * b = ${calculator.multiply(a,b)}`);
